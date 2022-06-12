3144ff622a2d1163d0f80076200d4c5ced1e6efd
idonag <idonag@post.bgu.ac.il>
2022-06-12 09:59:40 +0300

    changes

---
+++ src/BusinessLayer/GameManeger.java
@@ -0,0 +1,11 @@
+package BusinessLayer;
+
+import java.util.Collections;
+
+public class GameManeger {
+    private Player player;
+    private Collections enemies;
+    public void tick(){
+
+    }
+}
